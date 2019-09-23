7618ebcd254e5e10e749fe5c8a17fa2224e6a2b6
Phillip Potter <phil@philpotter.co.uk>
2019-09-24 00:17:37 +0100

    Nearing completion of port. Next step will be XML export.

--- gp_parser.cpp
+++ gp_parser.cpp
@@ -4,8 +4,10 @@
 #include <fstream>
 #include <iterator>
 #include <regex>
+#include <algorithm>
+#include <cstdio>
+#include <cmath>
 #include "gp_parser.h"
-#include <iostream>
 
 namespace gp_parser {
 
@@ -103,7 +105,7 @@ Parser::Parser(const char *filePath)
 		MeasureHeader header;
 		header.number = i + 1;
 		header.start = 0;
-		header.tempo = 120;
+		header.tempo.value = 120;
 		header.repeatOpen = (flags & 0x04) != 0;
 		if ((flags & 0x01) != 0)
 			timeSignature.numerator = readByte();
@@ -127,7 +129,7 @@ Parser::Parser(const char *filePath)
 			skip(4);
 		if ((flags & 0x10) == 0)
 			skip(1);
-		std::int8_t tripletFeel = readByte();
+		auto tripletFeel = readByte();
 		if (tripletFeel == 1)
 			header.tripletFeel = "eigth";
 		else if (tripletFeel == 2)
@@ -137,6 +139,59 @@ Parser::Parser(const char *filePath)
 
 		// Push header to vector
 		measureHeaders.push_back(header);
+	}
+
+	// Read tracks
+	for (auto number = 1; number <= trackCount; ++number) {
+		Track track;
+		readUnsignedByte();
+		if (number == 1 || versionIndex == 0)
+			skip(1);
+		track.number = number;
+		track.lyrics = number == lyricTrack ? lyric : Lyric();
+		track.name = readStringByte(40);
+		auto stringCount = readInt();
+		for (auto i = 0; i < 7; ++i) {
+			auto tuning = readInt();
+			if (stringCount > i) {
+				GuitarString string;
+				string.number = i + 1;
+				string.value = tuning;
+				track.strings.push_back(string);
+			}
+		}
+		readInt();
+		readChannel(track);
+		readInt();
+		track.offset = readInt();
+		track.color = readColor();
+		skip(versionIndex > 0 ? 49 : 44);
+		if (versionIndex > 0) {
+			readStringByteSizeOfInteger();
+			readStringByteSizeOfInteger();
+		}
+		tracks.push_back(track);
+	}
+	skip(versionIndex == 0 ? 2 : 1);
+
+	// Iterate through measures
+	Tempo tempo;
+	tempo.value = tempoValue;
+	auto start = QUARTER_TIME;
+	for (auto i = 0; i < measures; ++i) {
+		auto& header = measureHeaders[i];
+		header.start = start;
+		for (auto j = 0; j < trackCount; ++j) {
+			Track& track = tracks[j];
+			Measure measure;
+			measure.header = &header;
+			measure.start = start;
+			track.measures.push_back(measure);
+			readMeasure(measure, track, tempo, keySignature);
+			skip(1);
+		}
+		header.tempo = tempo;
+		start += getLength(header);
 	}
 }
 
@@ -305,7 +360,7 @@ std::vector<Channel> Parser::readChannels()
 	return channels;
 }
 
-// Read a color value
+/* Read a color value */
 Color Parser::readColor()
 {
 	Color c;
@@ -315,6 +370,538 @@ Color Parser::readColor()
 	skip(1);
 
 	return c;
+}
+
+/* Read a channel */
+void Parser::readChannel(Track& track)
+{
+	auto gmChannel1 = readInt() - 1;
+	auto gmChannel2 = readInt() - 1;
+	if (gmChannel1 >= 0 && gmChannel1 < channels.size()) {
+		// Allocate temporary buffer to hold chars for conversion
+		ChannelParam gmChannel1Param, gmChannel2Param;
+		gmChannel1Param.key = "gm channel 1";
+		gmChannel1Param.value.resize(numOfDigits(gmChannel1));
+		std::sprintf(&gmChannel1Param.value[0], "%d", gmChannel1);
+		gmChannel2Param.key = "gm channel 2";
+		gmChannel2Param.value.resize(numOfDigits(gmChannel1 != 9 ? gmChannel2 : gmChannel1));
+		std::sprintf(&gmChannel2Param.value[0], "%d", gmChannel1 != 9 ? gmChannel2 : gmChannel1);
+
+		// Copy channel to temporary variable
+		Channel channel = channels[gmChannel1];
+
+		// TODO: channel auxiliary, JS code below:
+		/*for (let i = 0; i < channels.length; i++) {
+			let channelAux = channels[i];
+			for (let n = 0; n < channelAux.; i++) {
+
+			}
+		}*/
+
+		if (channel.id == 0) {
+			channel.id = channels.size() + 1;
+			channel.name = "TODO";
+			channel.parameters.push_back(gmChannel1Param);
+			channel.parameters.push_back(gmChannel2Param);
+			channels.push_back(channel);
+		}
+		track.channelId = channel.id;
+	}
+}
+
+/* Read a measure */
+void Parser::readMeasure(Measure& measure, Track& track, Tempo& tempo, std::int8_t keySignature)
+{
+	for (auto voice = 0; voice < 2; ++voice) {
+		auto start = measure.start;
+		auto beats = readInt();
+		for (auto k = 0; k < beats; ++k)
+			start += readBeat(start, measure, track, tempo, voice);
+	}
+
+	std::vector<Beat*> emptyBeats;
+	for (auto i = 0; i < measure.beats.size(); ++i) {
+		auto beatPtr = &measure.beats[i];
+		auto empty = true;
+		for (auto v = 0; v < beatPtr->voices.size(); ++v) {
+			if (beatPtr->voices[v].notes.size() != 0)
+				empty = false;
+		}
+		if (empty)
+			emptyBeats.push_back(beatPtr);
+	}
+	for (auto beatPtr : emptyBeats) {
+		for (auto i = 0; i < measure.beats.size(); ++i) {
+			if (beatPtr == &measure.beats[i]) {
+				measure.beats.erase(measure.beats.begin() + i);
+				break;
+			}
+		}
+	}
+	measure.clef = getClef(track);
+	measure.keySignature = keySignature;
+}
+
+/* Get measure length */
+std::int32_t Parser::getLength(MeasureHeader& header)
+{
+	return 1;
+}
+
+/* Adds a new measure to the beat */
+Beat& Parser::getBeat(Measure& measure, std::int32_t start)
+{
+	for (auto& beat : measure.beats) {
+		if (beat.start == start)
+			return beat;
+	}
+
+	Beat beat;
+	beat.start = start;
+	measure.beats.push_back(beat);
+
+	return measure.beats[measure.beats.size() - 1];
+}
+
+/* Read mix change */
+void Parser::readMixChange(Tempo& tempo)
+{
+	readByte(); // instrument
+
+	skip(16);
+	auto volume = readByte();
+	auto pan = readByte();
+	auto chorus = readByte();
+	auto reverb = readByte();
+	auto phaser = readByte();
+	auto tremolo = readByte();
+	readStringByteSizeOfInteger(); // tempoName
+	auto tempoValue = readInt();
+	if (volume >= 0)
+		readByte();
+	if (pan >= 0)
+		readByte();
+	if (chorus >= 0)
+		readByte();
+	if (reverb >= 0)
+		readByte();
+	if (phaser >= 0)
+		readByte();
+	if (tremolo >= 0)
+		readByte();
+	if (tempoValue >= 0) {
+		tempo.value = tempoValue;
+		skip(1);
+		if (versionIndex > 0)
+			skip(1);
+	}
+	readByte();
+	skip(1);
+	if (versionIndex > 0) {
+		readStringByteSizeOfInteger();
+		readStringByteSizeOfInteger();
+	}
+}
+
+/* Read beat effects */
+void Parser::readBeatEffects(Beat& beat, NoteEffect& noteEffect)
+{
+	auto flags1 = readUnsignedByte();
+	auto flags2 = readUnsignedByte();
+	noteEffect.fadeIn = (flags1 & 0x10) != 0;
+	noteEffect.vibrato = (flags1 & 0x02) != 0;
+	if ((flags1 & 0x20) != 0) {
+		auto effect = readUnsignedByte();
+		noteEffect.tapping = effect == 1;
+		noteEffect.slapping = effect == 2;
+		noteEffect.popping = effect == 3;
+	}
+	if ((flags2 & 0x04) != 0)
+		readTremoloBar(noteEffect);
+	if ((flags1 & 0x40) != 0) {
+		auto strokeUp = readByte();
+		auto strokeDown = readByte();
+		// TODO
+		if (strokeUp > 0) {
+			beat.stroke.direction = "stroke_up";
+			beat.stroke.value = "stroke_down";
+		} else if (strokeDown > 0) {
+			beat.stroke.direction = "stroke_down";
+			beat.stroke.value = "stroke_down";
+		}
+	}
+	if ((flags2 & 0x02) != 0)
+		readByte();
+}
+
+/* Read tremolo bar */
+void Parser::readTremoloBar(NoteEffect& effect)
+{
+	skip(5);
+	TremoloBar tremoloBar;
+	auto numPoints = readInt();
+	for (auto i = 0; i < numPoints; ++i) {
+		auto position = readInt();
+		auto value = readInt();
+		readByte();
+
+		TremoloPoint point;
+		point.pointPosition = static_cast<std::int32_t>(std::round(
+				position * 1.0 /*'max position length'*/ / 
+				1.0 /*'bend position'*/)); // TODO
+		point.pointValue = static_cast<std::int32_t>(std::round(
+				value / (1.0/*'GP_BEND_SEMITONE'*/
+				* 0x2f))); //TODO
+		tremoloBar.points.push_back(point);
+	}
+	if (tremoloBar.points.size() > 0)
+		effect.tremoloBar = tremoloBar;
+}
+
+/* Read beat text */
+void Parser::readText(Beat& beat)
+{
+	beat.text.value = readStringByteSizeOfInteger();
+}
+
+/* Read chord */
+void Parser::readChord(std::vector<GuitarString>& strings, Beat& beat)
+{
+	Chord chord;
+	chord.strings = &strings;
+	skip(17);
+	chord.name = readStringByte(21);
+	skip(4);
+	chord.frets.resize(6);
+	chord.frets[0] = readInt();
+	for (auto i = 0; i < 7; ++i) {
+		auto fret = readInt();
+		if (i < chord.strings->size())
+			chord.frets[i] = fret;
+	}
+	skip(32);
+	if (chord.strings->size() > 0)
+		beat.chord = chord;
+}
+
+/* Get duration */
+double Parser::getTime(Duration& duration)
+{
+	auto time = QUARTER_TIME * 4.0 / duration.value;
+	if (duration.dotted)
+		time += time / 2;
+	else if (duration.doubleDotted)
+		time += (time / 4) * 3;
+
+	return time * duration.division.times / duration.division.enters;
+}
+
+/* Read duration */
+double Parser::readDuration(std::uint8_t flags)
+{
+	Duration duration;
+	duration.value = pow(2, (readByte() + 4)) / 4;
+	duration.dotted = (flags & 0x01) != 0;
+	if ((flags & 0x20) != 0) {
+		auto divisionType = readInt();
+		switch (divisionType) {
+		case 3:
+			duration.division.enters = 3;
+			duration.division.times = 2;
+			break;
+		case 5:
+			duration.division.enters = 5;
+			duration.division.times = 5;
+			break;
+		case 6:
+			duration.division.enters = 6;
+			duration.division.times = 4;
+			break;
+		case 7:
+			duration.division.enters = 7;
+			duration.division.times = 4;
+			break;
+		case 9:
+			duration.division.enters = 9;
+			duration.division.times = 8;
+			break;
+		case 10:
+			duration.division.enters = 10;
+			duration.division.times = 8;
+			break;
+		case 11:
+			duration.division.enters = 11;
+			duration.division.times = 8;
+			break;
+		case 12:
+			duration.division.enters = 12;
+			duration.division.times = 8;
+			break;
+		case 13:
+			duration.division.enters = 13;
+			duration.division.times = 8;
+			break;
+		}
+	}
+	if (duration.division.enters == 0) {
+		duration.division.enters = 1;
+		duration.division.times = 1;
+	}
+
+	return getTime(duration);
+}
+
+/* Read beat */
+double Parser::readBeat(std::int32_t start, Measure& measure, Track& track, Tempo& tempo, std::size_t voiceIndex)
+{
+	auto flags = readUnsignedByte();
+
+	auto& beat = getBeat(measure, start);
+	auto& voice = beat.voices[voiceIndex];
+	if ((flags & 0x40) != 0) {
+		auto beatType = readUnsignedByte();
+		voice.empty = (beatType & 0x02) == 0;
+	}
+	auto duration = readDuration(flags);
+	NoteEffect effect;
+	if ((flags & 0x02) != 0)
+		readChord(track.strings, beat);
+	if ((flags & 0x04) != 0)
+		readText(beat);
+	if ((flags & 0x08) != 0)
+		readBeatEffects(beat, effect);
+	if ((flags & 0x10) != 0)
+		readMixChange(tempo);
+	auto stringFlags = readUnsignedByte();
+	for (auto i = 6; i >= 0; --i) {
+		if ((stringFlags & (1 << i)) != 0 && (6 - i) < track.strings.size()) {
+			auto string = track.strings[6 - i];
+			auto note = readNote(string, track, effect);
+			voice.notes.push_back(note);
+		}
+		voice.duration = duration;
+	}
+
+	skip(1);
+
+	auto read = readByte();
+	if ((read & 0x02) != 0)
+		skip(1);
+
+	return (voice.notes.size() != 0 ? duration : 0);
+}
+
+/* Read note */
+Note Parser::readNote(GuitarString& string, Track& track, NoteEffect& effect)
+{
+	auto flags = readUnsignedByte();
+	Note note;
+	note.string = string.number;
+	note.effect = effect;
+	note.effect.accentuatedNote = (flags & 0x40) != 0;
+	note.effect.heavyAccentuatedNote = (flags & 0x02) != 0;
+	note.effect.ghostNote = (flags & 0x04) != 0;
+	if ((flags & 0x20) != 0) {
+		auto noteType = readUnsignedByte();
+		note.tiedNote = noteType == 0x02;
+		note.effect.deadNote = noteType == 0x03;
+	}
+	if ((flags & 0x10) != 0) {
+		note.velocity = TGVELOCITIES_MIN_VELOCITY +
+		(TGVELOCITIES_VELOCITY_INCREMENT * readByte()) -
+		TGVELOCITIES_VELOCITY_INCREMENT; // TODO
+	}
+	if ((flags & 0x20) != 0) {
+		auto fret = readByte();
+		auto value = note.tiedNote
+			? getTiedNoteValue(string.number, track)
+			: fret;
+		note.value = value >= 0 && value < 100
+			? value
+			: 0;
+	}
+	if ((flags & 0x80) != 0)
+		skip(2);
+	if ((flags & 0x01) != 0)
+		skip(8);
+	skip(1);
+	if ((flags & 0x08) != 0)
+		readNoteEffects(note.effect);
+
+	return note;
+}
+
+/* Get tied note value */
+std::int8_t Parser::getTiedNoteValue(std::int32_t string, Track& track)
+{
+	auto measureCount = track.measures.size();
+	if (measureCount > 0) {
+		for (auto m = measureCount - 1; m >= 0; --m) {
+			auto& measure = track.measures[m];
+			for (auto b = measure.beats.size() - 1; b >= 0; --b) {
+				auto& beat = measure.beats[b];
+				for (auto v = 0; v < beat.voices.size(); ++v) {
+					auto& voice = beat.voices[v];
+					if (!voice.empty) {
+						for (auto n = 0; n < voice.notes.size(); ++n) {
+							auto& note = voice.notes[n];
+							if (note.string == string)
+								return note.value;
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+/* Read effects for note */
+void Parser::readNoteEffects(NoteEffect& noteEffect)
+{
+	auto flags1 = readUnsignedByte();
+	auto flags2 = readUnsignedByte();
+	if ((flags1 & 0x01) != 0)
+		readBend(noteEffect);
+	if ((flags1 & 0x10) != 0)
+		readGrace(noteEffect);
+	if ((flags2 & 0x04) != 0)
+		readTremoloPicking(noteEffect);
+	if ((flags2 & 0x08) != 0) {
+		noteEffect.slide = true;
+		readByte();
+	}
+	if ((flags2 & 0x10) != 0)
+		readArtificialHarmonic(noteEffect);
+	if ((flags2 & 0x20) != 0)
+		readTrill(noteEffect);
+	noteEffect.hammer = (flags1 & 0x02) != 0;
+	noteEffect.letRing = (flags1 & 0x08) != 0;
+	noteEffect.vibrato = (flags2 & 0x40) != 0;
+	noteEffect.palmMute = (flags2 & 0x02) != 0;
+	noteEffect.staccato = (flags2 & 0x01) != 0;
+}
+
+/* Read bend */
+void Parser::readBend(NoteEffect& effect)
+{
+	skip(5);
+	Bend bend;
+	auto numPoints = readInt();
+	for (auto i = 0; i < numPoints; ++i) {
+		auto bendPosition = readInt();
+		auto bendValue = readInt();
+		readByte();
+		BendPoint p;
+		p.pointPosition = std::round(bendPosition *
+				TGEFFECTBEND_MAX_POSITION_LENGTH /
+				static_cast<double>(GP_BEND_POSITION));
+		p.pointValue = std::round(bendValue *
+				TGEFFECTBEND_SEMITONE_LENGTH /
+				static_cast<double>(GP_BEND_SEMITONE));
+		bend.points.push_back(p);
+	}
+	if (bend.points.size() > 0)
+		effect.bend = bend;
+}
+
+/* Read grace */
+void Parser::readGrace(NoteEffect& effect)
+{
+	auto fret = readUnsignedByte();
+	auto dynamic = readUnsignedByte();
+	auto transition = readByte();
+	auto duration = readUnsignedByte();
+	auto flags = readUnsignedByte();
+	Grace grace;
+	grace.fret = fret;
+	grace.dynamic = (TGVELOCITIES_MIN_VELOCITY +
+			(TGVELOCITIES_VELOCITY_INCREMENT * dynamic)) -
+			TGVELOCITIES_VELOCITY_INCREMENT;
+	grace.duration = duration;
+	grace.dead = (flags & 0x01) != 0;
+	grace.onBeat = (flags & 0x02) != 0;
+	if (transition == 0)
+		grace.transition = "none";
+	else if (transition == 1)
+		grace.transition = "slide";
+	else if (transition == 2)
+		grace.transition = "bend";
+	else if (transition == 3)
+		grace.transition = "hammer";
+	effect.grace = grace;
+}
+
+/* Read tremolo picking */
+void Parser::readTremoloPicking(NoteEffect& effect)
+{
+	auto value = readUnsignedByte();
+	TremoloPicking tp;
+	if (value == 1) {
+		tp.duration.value = "eigth";
+		effect.tremoloPicking = tp;
+	} else if (value == 2) {
+		tp.duration.value = "sixteenth";
+		effect.tremoloPicking = tp;
+	} else if (value == 3) {
+		tp.duration.value = "thirty_second";
+		effect.tremoloPicking = tp;
+	}
+}
+
+/* Read artificial harmonic */
+void Parser::readArtificialHarmonic(NoteEffect& effect)
+{
+	auto type = readByte();
+	Harmonic harmonic;
+	if (type == 1) {
+		harmonic.type = "natural";
+		effect.harmonic = harmonic;
+	} else if (type == 2) {
+		skip(3);
+		harmonic.type = "artificial";
+		effect.harmonic = harmonic;
+	} else if (type == 3) {
+		skip(1);
+		harmonic.type = "tapped";
+		effect.harmonic = harmonic;
+	} else if (type == 4) {
+		harmonic.type = "pinch";
+		effect.harmonic = harmonic;
+	} else if (type == 5) {
+		harmonic.type = "semi";
+		effect.harmonic = harmonic;
+	}
+}
+
+/* Read trill */
+void Parser::readTrill(NoteEffect& effect)
+{
+	auto fret = readByte();
+	auto period = readByte();
+	Trill trill;
+	trill.fret = fret;
+	if (period == 1) {
+		trill.duration.value = "sixteenth";
+		effect.trill = trill;
+	} else if (period == 2) {
+		trill.duration.value = "thirty_second";
+		effect.trill = trill;
+	} else if (period == 3) {
+		trill.duration.value = "sixty_fourth";
+		effect.trill = trill;
+	}
+}
+
+/* Tells us how many digits there are in a base 10 number */
+std::int32_t numOfDigits(std::int32_t num)
+{
+	auto digits = 0;
+	for (auto order = 1; num / order != 0; order *= 10)
+		++digits;
+
+	return digits;
 }
 
 }
--- gp_parser.h
+++ gp_parser.h
@@ -17,6 +17,12 @@ static const char *VERSIONS[] = {
 };
 static const int QUARTER_TIME = 960;
 static const int QUARTER = 4;
+static const int TGEFFECTBEND_MAX_POSITION_LENGTH = 12;
+static const int TGEFFECTBEND_SEMITONE_LENGTH = 1;
+static const int GP_BEND_SEMITONE = 25;
+static const int GP_BEND_POSITION = 60;
+static const int TGVELOCITIES_MIN_VELOCITY = 15;
+static const int TGVELOCITIES_VELOCITY_INCREMENT = 16;
 
 // Define struct to hold lyrics data
 struct Lyric {
@@ -24,8 +30,16 @@ struct Lyric {
 	std::string lyric;
 };
 
+// Define channel parameter struct
+struct ChannelParam {
+	std::string key;
+	std::string value;
+};
+
 // Define channel struct
 struct Channel {
+	std::int32_t id;
+	std::string name;
 	std::int32_t program;
 	std::int8_t volume;
 	std::int8_t balance;
@@ -34,6 +48,7 @@ struct Channel {
 	std::int8_t phaser;
 	std::int8_t tremolo;
 	std::string bank;
+	std::vector<ChannelParam> parameters;
 };
 
 // Define division struct
@@ -68,17 +83,177 @@ struct Marker {
 	Color color;	
 };
 
+// Define tempo struct
+struct Tempo {
+	std::int32_t value;
+};
+
 // Define measure header struct
 struct MeasureHeader {
 	std::int32_t number;
 	std::int32_t start;
-	std::int32_t tempo;
+	Tempo tempo;
 	bool repeatOpen;
 	TimeSignature timeSignature;
 	std::int8_t repeatClose;
 	Marker marker;
 	std::uint8_t repeatAlternative;
 	std::string tripletFeel;
+};
+
+// Define tremolo point struct
+struct TremoloPoint {
+	std::int32_t pointPosition;
+	std::int32_t pointValue;
+};
+
+// Define tremolo bar struct
+struct TremoloBar {
+	std::vector<TremoloPoint> points;
+};
+
+// Define bend point struct
+struct BendPoint {
+	std::int32_t pointPosition;
+	std::int32_t pointValue;
+};
+
+// Define bend struct
+struct Bend {
+	std::vector<BendPoint> points;
+};
+
+// Define grace struct
+struct Grace {
+	std::uint8_t fret;
+	std::int32_t dynamic;
+	std::string transition;
+	std::uint8_t duration;
+	bool dead;
+	bool onBeat;
+};
+
+// Define effect duration struct
+struct EffectDuration {
+	std::string value;
+};
+
+// Define tremolo picking struct
+struct TremoloPicking {
+	EffectDuration duration;
+};
+
+// Define harmonic struct
+struct Harmonic {
+	std::int32_t data;
+	std::string type;
+};
+
+// Define trill struct
+struct Trill {
+	std::int8_t fret;
+	EffectDuration duration;
+};
+
+// Define note effect struct
+struct NoteEffect {
+	bool fadeIn;
+	bool vibrato;
+	bool tapping;
+	bool slapping;
+	bool popping;
+	bool deadNote;
+	bool accentuatedNote;
+	bool heavyAccentuatedNote;
+	bool ghostNote;
+	bool slide;
+	bool hammer;
+	bool letRing;
+	bool palmMute;
+	bool staccato;
+	TremoloBar tremoloBar;
+	TremoloPicking tremoloPicking;
+	Bend bend;
+	Grace grace;
+	Harmonic harmonic;
+	Trill trill;
+};
+
+// Define note struct
+struct Note {
+	std::int32_t string;
+	NoteEffect effect;
+	bool tiedNote;
+	std::int8_t value;
+	std::int32_t velocity;
+};
+
+// Define voice struct
+struct Voice {
+	std::vector<Note> notes;
+	double duration;
+	bool empty;
+};
+
+// Define stroke struct
+struct Stroke {
+	std::string direction;
+	std::string value;
+};
+
+// Define guitar string struct
+struct GuitarString {
+	std::int32_t number;
+	std::int32_t value;
+};
+
+// Define chord struct
+struct Chord {
+	std::string name;
+	std::vector<GuitarString>* strings;
+	std::vector<std::int32_t> frets;
+};
+
+// Define beat text struct
+struct BeatText {
+	std::string value;
+};
+
+// Define beat struct
+struct Beat {
+	std::int32_t start;
+	std::vector<Voice> voices;
+	Stroke stroke;
+	BeatText text;
+	Chord chord;
+};
+
+// Define measure struct
+struct Measure {
+	MeasureHeader *header;
+	std::int32_t start;
+	std::vector<Beat> beats;
+	std::int8_t keySignature;
+};
+
+// Define track struct
+struct Track {
+	std::int32_t channelId;
+	std::int32_t number;
+	Lyric lyrics;
+	std::string name;
+	std::vector<GuitarString> strings;
+	std::int32_t offset;
+	Color color;
+	std::vector<Measure> measures;
+};
+
+// Define duration struct
+struct Duration {
+	double value;
+	bool dotted;
+	bool doubleDotted;
+	Division division;
 };
 
 class Parser {
@@ -109,6 +284,7 @@ private:
 	std::int32_t measures;
 	std::int32_t trackCount;
 	std::vector<MeasureHeader> measureHeaders;
+	std::vector<Track> tracks;
 
 	// Private member functions for reading low-level file data
 	std::uint8_t readUnsignedByte();
@@ -129,7 +305,29 @@ private:
 	std::int8_t readKeySignature();
 	std::vector<Channel> readChannels();
 	Color readColor();
+	void readChannel(Track& track);
+	void readMeasure(Measure& measure, Track& track, Tempo& tempo, std::int8_t keySignature);
+	std::int32_t getLength(MeasureHeader& header);
+	Beat& getBeat(Measure& measure, std::int32_t start);
+	void readMixChange(Tempo& tempo);
+	void readBeatEffects(Beat& beat, NoteEffect& noteEffect);
+	void readTremoloBar(NoteEffect& effect);
+	void readText(Beat& beat);
+	void readChord(std::vector<GuitarString>& strings, Beat& beat);
+	double getTime(Duration& duration);
+	double readDuration(std::uint8_t flags);
+	double readBeat(std::int32_t start, Measure& measure, Track& track, Tempo& tempo, std::size_t voiceIndex);
+	Note readNote(GuitarString& string, Track& track, NoteEffect& effect);
+	std::int8_t getTiedNoteValue(std::int32_t string, Track& track);
+	void readNoteEffects(NoteEffect& noteEffect);
+	void readBend(NoteEffect& effect);
+	void readGrace(NoteEffect& effect);
+	void readTremoloPicking(NoteEffect& effect);
+	void readArtificialHarmonic(NoteEffect& effect);
+	void readTrill(NoteEffect& effect);
 };
+
+std::int32_t numOfDigits(std::int32_t num);
 
 }
 
